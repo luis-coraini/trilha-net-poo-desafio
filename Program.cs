47bca66aae5513b7989c39e35c91369bf01c4a38
Luis Felipe Coraini Santos <luisfelipe.coraini@gmail.com>
2024-05-20 21:40:54 -0300

    Projeto concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia: ");
+Nokia nokia = new Nokia("1234312", "Modelo 1", "111111111", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone: ");
+Iphone iphone = new Iphone("43653121", "Modelo 2", "2222222222", 256);
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Telegram");
+
